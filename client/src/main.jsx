f196b6ee5f9bd906928b00666a06f2b378f564c1
VMendoza2603 <vmendozam@unemi.edu.ec>
2026-07-12 21:34:30 -0500

    Cambio de login y particularidades

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
-import './styles/index.css';
+import './styles/variables.css';
+import './styles/reset.css';
+import './styles/globals.css';
+import './styles/layout.css';
+import './styles/buttons.css';
+import './styles/forms.css';
+import './styles/cards.css';
+import './styles/dashboard.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
